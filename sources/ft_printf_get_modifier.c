e272bc34792eaee51031db5b57f3243ab4013e6f
Mathis Pandelé <mapandel@student.42.fr>
2017-04-07 16:19:39 +0200

    ft_printf_get_modifier 3.0 : Retake

---
+++ sources/ft_printf_get_modifier.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_get_modifier.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/06 22:17:15 by mapandel          #+#    #+#             */
+/*   Updated: 2017/04/07 14:57:44 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void			ft_printf_get_modifier(t_printf *p, const char *format)
+{
+	if (format[p->index] == 'h')
+	{
+		p->modifier = FT_PRINTF_H;
+		if (format[p->index + 1] == 'h' && ++p->index)
+			p->modifier = FT_PRINTF_HH;
+	}
+	else if (format[p->index] == 'j')
+		p->modifier = FT_PRINTF_J;
+	else if (format[p->index] == 'l')
+	{
+		p->modifier = FT_PRINTF_L;
+		if (format[p->index + 1] == 'l' && ++p->index)
+			p->modifier = FT_PRINTF_LL;
+	}
+	else if (format[p->index] == 'z')
+		p->modifier = FT_PRINTF_Z;
+	else
+		return ;
+	++p->index;
+}
