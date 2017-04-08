177b99bb51e2fa12acc4e6f174e74b74b63bbe94
Mathis PANDELE <mapandel@e2r3p16.42.fr>
2017-04-08 18:46:43 +0200

    ft_printf_parsing.c 3.2 : Parsing done

--- sources/ft_printf_parsing.c
+++ sources/ft_printf_parsing.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/30 19:57:38 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/08 01:44:55 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/08 18:38:01 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,11 @@ t_printf		*ft_printf_parsing(t_printf *p, const char *format)
 			ft_printf_get_flag(p, format);
 		else if (format[p->index] == '.')
 			ft_printf_get_precision(p, format);
-//		else if (ft_isdigit((int)format[p->index]) && format[p->index] != '0')
-//			ft_printf_get_width(p, format);
-		else
-		{
-			p->error = -1;
+		else if ((format[p->index] != '0' && ft_isdigit((int)format[p->index]))
+			|| format[p->index] == '*')
+			ft_printf_get_width(p, format);
+		else if (!(p->error = -1))
 			return (p);
-		}
 	}
 	ft_printf_get_conv(p, format);
 	if (p->conv == FT_PRINTF_WAIT_INPUT)
