4a2caa7fc721a210cf7eab5b54227ad6a3b81195
Mathis Pandelé <mapandel@student.42.fr>
2017-04-08 15:34:54 +0200

    touch_t_printf.c 3.1 : More reset

--- sources/touch_t_printf.c
+++ sources/touch_t_printf.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/30 10:27:37 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/07 14:58:32 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/07 17:11:12 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,28 @@
 
 void			del_t_printf(t_printf *p)
 {
+	ft_memdel((void**)&p->flags);
 	ft_memdel((void**)&p);
 }
 
 void			reset_t_printf(t_printf *p)
 {
+	p->flags->sharp = 0;
+	p->flags->zero = 0;
+	p->flags->less = 0;
+	p->flags->space = 0;
+	p->flags->plus = 0;
+	p->flags->sharp = 0;
+	p->flags->apostrophe = 0;
+	p->precision = -1;
 	p->modifier = FT_PRINTF_NO_MODIFIERS;
 	p->conv = FT_PRINTF_WAIT_INPUT;
 }
 
 t_printf		*init_t_printf(t_printf *p)
 {
-	if (!p && !(p = ft_memalloc(sizeof(p))))
+	if (!(p = ft_memalloc(sizeof(t_printf)))
+		|| !(p->flags = ft_memalloc(sizeof(t_printf_flags))))
 		exit (-1);
 	reset_t_printf(p);
 	p->error = 0;
