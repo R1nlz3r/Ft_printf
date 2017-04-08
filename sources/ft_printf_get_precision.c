e4088816d7bcf3fba11b4ab0cf9b2b55831d070b
Mathis PANDELE <mapandel@e2r3p16.42.fr>
2017-04-08 16:46:21 +0200

    ft_printf_get_precision.c 3.1 : Working

--- sources/ft_printf_get_precision.c
+++ sources/ft_printf_get_precision.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 01:57:24 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/08 01:57:37 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/08 16:28:45 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,19 @@
 void			ft_printf_get_precision(t_printf *p, const char *format)
 {
 	int		tmp;
+	char	tmp_intmax_buffer;
 	int		i;
 
 	i = 0;
 	++p->index;
-	while (format[p->index] == '0')
-		++p->index;
-	if ((tmp = ft_atoi(&format[p->index]) < 0)
-		|| !ft_isdigit(format[p->index]))
-	{
-		ft_putnbr(tmp);
-		p->precision = 0;
-		return ;
-	}
-	ft_putnbr(tmp);
-	p->precision = tmp;
+	tmp = ft_atoi(&format[p->index]);
+	tmp_intmax_buffer = format[p->index];
+	while (ft_isdigit(format[p->index + i]) && i < 11)
+		++i;
 	while (ft_isdigit(format[p->index]))
 		++p->index;
+	if (tmp < 0 || i >= 11 || (i == 10 && tmp_intmax_buffer > '2')
+		|| !ft_isdigit(format[p->index]))
+		return ;
+	p->precision = tmp;
 }
