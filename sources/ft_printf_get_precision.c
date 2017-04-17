b778f3930372cab3218bbf5a3f9cdfa21401b5cc
Mathis Pandelé <mapandel@student.42.fr>
2017-04-18 01:02:37 +0200

    ft_printf_get_precision.c 3.3 : Negative * Fix

--- sources/ft_printf_get_precision.c
+++ sources/ft_printf_get_precision.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 01:57:24 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/08 18:32:41 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/17 06:22:32 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void		ft_printf_get_star_precision(t_printf *p)
 	star = va_arg(p->ap, int);
 	p->precision = star;
 	if (star < 0)
-		p->precision = 0;
+		p->precision = -1;
 }
 
 void			ft_printf_get_precision(t_printf *p, const char *format)
