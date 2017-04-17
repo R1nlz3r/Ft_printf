ec02c51fa4545144c2717c5d4fae3274980af16d
Mathis Pandelé <mapandel@student.42.fr>
2017-04-18 01:01:28 +0200

    ft_printf_get_modifier.c 3.1 : Fixes

--- sources/ft_printf_get_modifier.c
+++ sources/ft_printf_get_modifier.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/06 22:17:15 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/07 23:03:54 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/17 20:50:06 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ void			ft_printf_get_modifier(t_printf *p, const char *format)
 {
 	if (format[p->index] == 'h')
 	{
-		p->modifier = FT_PRINTF_H;
-		if (format[p->index + 1] == 'h' && ++p->index)
+		if (p->modifier == FT_PRINTF_H)
 			p->modifier = FT_PRINTF_HH;
+		else if (p->modifier < FT_PRINTF_H)
+			p->modifier = FT_PRINTF_H;
 	}
-	else if (format[p->index] == 'j')
+	else if (format[p->index] == 'j' && p->modifier < FT_PRINTF_J)
 		p->modifier = FT_PRINTF_J;
+	else if (format[p->index] == 'z' && p->modifier < FT_PRINTF_Z)
+		p->modifier = FT_PRINTF_Z;
 	else if (format[p->index] == 'l')
 	{
-		p->modifier = FT_PRINTF_L;
-		if (format[p->index + 1] == 'l' && ++p->index)
+		if (p->modifier == FT_PRINTF_L)
 			p->modifier = FT_PRINTF_LL;
+		else if (p->modifier < FT_PRINTF_L)
+			p->modifier = FT_PRINTF_L;
 	}
-	else if (format[p->index] == 'z')
-		p->modifier = FT_PRINTF_Z;
 	++p->index;
 }
