a2ca386957f7a1f4b6568312bdbd908e39bc663f
Mathis Pandelé <mapandel@student.42.fr>
2017-04-28 19:56:30 +0200

    ft_printf_conv.c 3.9 : + Conversion missing

--- sources/ft_printf_conv.c
+++ sources/ft_printf_conv.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/30 19:57:38 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/27 05:03:58 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/27 06:25:33 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_printf		*ft_printf_conv(t_printf *p)
 	int		i;
 
 	i = 0;
-	while (i < 10 && !p->error)
+	while (i < 11 && !p->error)
 		p = p->tab_ptr[i++](p);
 	return (p);
 }
